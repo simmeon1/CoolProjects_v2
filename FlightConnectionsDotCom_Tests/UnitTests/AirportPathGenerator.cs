98eb4ae43b69e3309b0a46158e40cc01c3887137
simmeon1 <simmeonn1@gmail.com>
2021-07-07 23:27:25 +0100

    Added AirportPathGenerator and test

---
+++ FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator.cs
@@ -0,0 +1,48 @@
+﻿using FlightConnectionsDotCom_ClassLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace FlightConnectionsDotCom_Tests.UnitTests
+{
+    public class AirportPathGenerator
+    {
+        private IEnumerable<Airport> Airports { get; set; }
+        private IDictionary<string, HashSet<string>> AirportDestinations { get; set; }
+
+        public AirportPathGenerator(IEnumerable<Airport> airports, IDictionary<string, HashSet<string>> airportDestinations)
+        {
+            Airports = airports;
+            AirportDestinations = airportDestinations;
+        }
+
+        public List<List<string>> GeneratePaths(string origin, string destination)
+        {
+            List<List<string>> paths = new();
+            ICollection<string> destinations1 = AirportDestinations[origin];
+            foreach (string destination1 in destinations1)
+            {
+                if (destination1.Equals(destination))
+                {
+                    paths.Add(new List<string>() { origin, destination1 });
+                }
+                ICollection<string> destinations2 = AirportDestinations[destination1];
+                foreach (string destination2 in destinations2)
+                {
+                    if (destination2.Equals(destination))
+                    {
+                        paths.Add(new List<string>() { origin, destination1, destination2 });
+                    }
+                    ICollection<string> destinations3 = AirportDestinations[destination2];
+                    foreach (string destination3 in destinations3)
+                    {
+                        if (destination3.Equals(destination))
+                        {
+                            paths.Add(new List<string>() { origin, destination1, destination2, destination3 });
+                        }
+                    }
+                }
+            }
+            return paths;
+        }
+    }
+}
---
+++ FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator_UnitTests.cs
@@ -0,0 +1,42 @@
+﻿using FlightConnectionsDotCom_ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace FlightConnectionsDotCom_Tests.UnitTests
+{
+    [TestClass]
+    public class AirportPathGenerator_UnitTests
+    {
+        [TestMethod]
+        public void GetAirportConnections_ReturnsExpectedValues()
+        {
+            const string codeABZ = "ABZ";
+            const string codeSOF = "SOF";
+            const string codeEDI = "EDI";
+            const string codeCIA = "CIA";
+            Airport airport1 = new(codeABZ, "Aberdeen", "United Kingdom", "Aberdeen Airport", "linkA");
+            Airport airport2 = new(codeSOF, "Sofia", "Bulgaria", "Sofia Airport", "linkB");
+            Airport airport3 = new(codeEDI, "Edinburgh", "United Kingdom", "Edinburgh Airport", "linkC");
+            Airport airport4 = new(codeCIA, "Rome", "Italy", "Rome Airport", "linkD");
+            ICollection<Airport> airportList = new HashSet<Airport>() { airport1, airport2, airport3, airport4 };
+
+            Dictionary<string, HashSet<string>> airportAndDestinationsList = new()
+            {
+                { codeABZ, new HashSet<string>() { codeSOF } },
+                { codeSOF, new HashSet<string>() { codeEDI } },
+                { codeEDI, new HashSet<string>() { codeSOF } }
+            };
+            AirportPathGenerator generator = new(airportList, airportAndDestinationsList);
+            List<List<string>> paths = generator.GeneratePaths(codeABZ, codeSOF);
+            Assert.IsTrue(paths.Count == 2);
+            Assert.IsTrue(paths[0].Count == 2);
+            Assert.IsTrue(paths[0][0].Equals(codeABZ));
+            Assert.IsTrue(paths[0][1].Equals(codeSOF));
+            Assert.IsTrue(paths[1].Count == 4);
+            Assert.IsTrue(paths[1][0].Equals(codeABZ));
+            Assert.IsTrue(paths[1][1].Equals(codeSOF));
+            Assert.IsTrue(paths[1][2].Equals(codeEDI));
+            Assert.IsTrue(paths[1][3].Equals(codeSOF));
+        }
+    }
+}
