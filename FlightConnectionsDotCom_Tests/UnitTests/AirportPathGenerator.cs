56aabb5fc10500f7473029941d63f0baa2456b88
simmeon1 <simmeonn1@gmail.com>
2021-07-08 00:11:38 +0100

    Improved generator code

--- FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator.cs
+++ FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator.cs
@@ -8,6 +8,9 @@ namespace FlightConnectionsDotCom_Tests.UnitTests
     {
         private IEnumerable<Airport> Airports { get; set; }
         private IDictionary<string, HashSet<string>> AirportDestinations { get; set; }
+        private LinkedList<string> CurrentPath { get; set; }
+        private int MaxFlights { get; set; }
+        private List<List<string>> Paths { get; set; }
 
         public AirportPathGenerator(IEnumerable<Airport> airports, IDictionary<string, HashSet<string>> airportDestinations)
         {
@@ -17,27 +20,33 @@ namespace FlightConnectionsDotCom_Tests.UnitTests
 
         public List<List<string>> GeneratePaths(string origin, string target, int maxFlights)
         {
-            List<List<string>> paths = new();
-            LinkedList<string> currentPath = new();
-            currentPath.AddLast(origin);
-            int depthScanned = 0;
-            GeneratePath(origin, target, paths, ref depthScanned, maxFlights, currentPath);
-            return paths;
+            Initialise(maxFlights);
+            ScanPathAndUpdateCurrentPath(origin, target);
+            return Paths;
         }
 
-        private void GeneratePath(string origin, string target, List<List<string>> paths, ref int depthScanned, int maxFlights, LinkedList<string> currentPath)
+        private void ScanPathAndUpdateCurrentPath(string origin, string target)
         {
-            while (depthScanned < maxFlights)
+            CurrentPath.AddLast(origin);
+            ScanPath(origin, target);
+            CurrentPath.RemoveLast();
+        }
+
+        private void Initialise(int maxFlights)
+        {
+            Paths = new List<List<string>>();
+            CurrentPath = new LinkedList<string>();
+            MaxFlights = maxFlights;
+        }
+
+        private void ScanPath(string origin, string target)
+        {
+            if (CurrentPath.Count - 1 >= MaxFlights) return;
+            HashSet<string> destinations = AirportDestinations[origin];
+            foreach (string destination in destinations)
             {
-                HashSet<string> destinations = AirportDestinations[origin];
-                foreach (string destination in destinations)
-                {
-                    if (destination.Equals(target)) paths.Add(new List<string>(currentPath) { destination });
-                    depthScanned++;
-                    currentPath.AddLast(destination);
-                    GeneratePath(destination, target, paths, ref depthScanned, maxFlights, currentPath);
-                    currentPath.RemoveLast();
-                }
+                if (destination.Equals(target)) Paths.Add(new List<string>(CurrentPath) { destination });
+                ScanPathAndUpdateCurrentPath(destination, target);
             }
         }
     }
