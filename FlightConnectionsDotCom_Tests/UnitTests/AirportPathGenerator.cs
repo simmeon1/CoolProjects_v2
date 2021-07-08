77951d34c5df96daa18ffb68099e833047970140
simmeon1 <simmeonn1@gmail.com>
2021-07-09 00:10:36 +0100

    Updated path generator

--- FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator.cs
+++ FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator.cs
@@ -1,52 +1,65 @@
 ﻿using FlightConnectionsDotCom_ClassLibrary;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FlightConnectionsDotCom_Tests.UnitTests
 {
     public class AirportPathGenerator
     {
-        private IEnumerable<Airport> Airports { get; set; }
         private IDictionary<string, HashSet<string>> AirportDestinations { get; set; }
         private LinkedList<string> CurrentPath { get; set; }
         private int MaxFlights { get; set; }
         private List<List<string>> Paths { get; set; }
+        private string Origin { get; set; }
+        private string Target { get; set; }
 
-        public AirportPathGenerator(IEnumerable<Airport> airports, IDictionary<string, HashSet<string>> airportDestinations)
+        public AirportPathGenerator(IDictionary<string, HashSet<string>> airportDestinations)
         {
-            Airports = airports;
             AirportDestinations = airportDestinations;
         }
 
         public List<List<string>> GeneratePaths(string origin, string target, int maxFlights)
         {
-            Initialise(maxFlights);
-            ScanPathAndUpdateCurrentPath(origin, target);
+            Initialise(origin, target, maxFlights);
+            UpdateCurrentPathAndScanItIfNeeded(origin, target);
             return Paths;
         }
 
-        private void ScanPathAndUpdateCurrentPath(string origin, string target)
+        private void UpdateCurrentPathAndScanItIfNeeded(string origin, string target)
         {
+            if (CurrentPath.Count >= MaxFlights || (CurrentPath.Last != null && CurrentPath.Last.Value.Equals(Target))) return;
+            
             CurrentPath.AddLast(origin);
-            ScanPath(origin, target);
+            Dictionary<string, int> airportOccurences = new();
+            foreach (string airport in CurrentPath)
+            {
+                if (airportOccurences.ContainsKey(airport)) airportOccurences[airport]++;
+                else airportOccurences.Add(airport, 1);
+            }
+            if (!airportOccurences.Values.Any(c => c > 1))
+            {
+                ScanCurrentPath(origin, target);
+            }
             CurrentPath.RemoveLast();
         }
 
-        private void Initialise(int maxFlights)
+        private void Initialise(string origin, string target, int maxFlights)
         {
             Paths = new List<List<string>>();
             CurrentPath = new LinkedList<string>();
             MaxFlights = maxFlights;
+            Origin = origin;
+            Target = target;
         }
 
-        private void ScanPath(string origin, string target)
+        private void ScanCurrentPath(string origin, string target)
         {
-            if (CurrentPath.Count - 1 >= MaxFlights) return;
             HashSet<string> destinations = AirportDestinations[origin];
             foreach (string destination in destinations)
             {
                 if (destination.Equals(target)) Paths.Add(new List<string>(CurrentPath) { destination });
-                ScanPathAndUpdateCurrentPath(destination, target);
+                UpdateCurrentPathAndScanItIfNeeded(destination, target);
             }
         }
     }
--- FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator_UnitTests.cs
+++ FlightConnectionsDotCom_Tests/UnitTests/AirportPathGenerator_UnitTests.cs
@@ -7,46 +7,88 @@ namespace FlightConnectionsDotCom_Tests.UnitTests
     [TestClass]
     public class AirportPathGenerator_UnitTests
     {
-        [TestMethod]
-        public void GetAirportConnections_ReturnsExpectedValues()
+        private const string codeABZ = "ABZ";
+        private const string codeSOF = "SOF";
+        private const string codeEDI = "EDI";
+        private const string codeCIA = "CIA";
+
+        Airport airport1;
+        Airport airport2;
+        Airport airport3;
+        Airport airport4;
+        ICollection<Airport> airportList;
+        Dictionary<string, HashSet<string>> airportAndDestinationsList;
+        AirportPathGenerator generator;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
-            const string codeABZ = "ABZ";
-            const string codeSOF = "SOF";
-            const string codeEDI = "EDI";
-            const string codeCIA = "CIA";
-            Airport airport1 = new(codeABZ, "Aberdeen", "United Kingdom", "Aberdeen Airport", "linkA");
-            Airport airport2 = new(codeSOF, "Sofia", "Bulgaria", "Sofia Airport", "linkB");
-            Airport airport3 = new(codeEDI, "Edinburgh", "United Kingdom", "Edinburgh Airport", "linkC");
-            Airport airport4 = new(codeCIA, "Rome", "Italy", "Rome Airport", "linkD");
-            ICollection<Airport> airportList = new HashSet<Airport>() { airport1, airport2, airport3, airport4 };
-
-            Dictionary<string, HashSet<string>> airportAndDestinationsList = new()
+            airport1 = new(codeABZ, "Aberdeen", "United Kingdom", "Aberdeen Airport", "linkA");
+            airport2 = new(codeSOF, "Sofia", "Bulgaria", "Sofia Airport", "linkB");
+            airport3 = new(codeEDI, "Edinburgh", "United Kingdom", "Edinburgh Airport", "linkC");
+            airport4 = new(codeCIA, "Rome", "Italy", "Rome Airport", "linkD");
+            airportList = new HashSet<Airport>() { airport1, airport2, airport3, airport4 };
+            airportAndDestinationsList = new()
             {
-                { codeABZ, new HashSet<string>() { codeSOF } },
-                { codeSOF, new HashSet<string>() { codeEDI } },
-                { codeEDI, new HashSet<string>() { codeSOF } }
+                { codeABZ, new HashSet<string>() { codeSOF, codeEDI, codeCIA } },
+                { codeSOF, new HashSet<string>() { codeEDI, codeCIA, codeABZ } },
+                { codeEDI, new HashSet<string>() { codeSOF, codeABZ, codeCIA } },
+                { codeCIA, new HashSet<string>() { codeSOF, codeABZ, codeEDI } }
             };
-            AirportPathGenerator generator = new(airportList, airportAndDestinationsList);
+            generator = new(airportAndDestinationsList);
+        }
 
+        [TestMethod]
+        public void GetAirportConnections_ReturnsZeroResultsWhenMaxFlightsAreZero()
+        {
             List<List<string>> paths = generator.GeneratePaths(codeABZ, codeSOF, 0);
             Assert.IsTrue(paths.Count == 0);
-            
-            paths = generator.GeneratePaths(codeABZ, codeSOF, 1);
+        }
+
+        [TestMethod]
+        public void GetAirportConnections_ReturnsExpectedOnePathWhenMaxFlightsAreOne()
+        {
+            List<List<string>> paths = generator.GeneratePaths(codeABZ, codeSOF, 1);
             Assert.IsTrue(paths.Count == 1);
             Assert.IsTrue(paths[0].Count == 2);
             Assert.IsTrue(paths[0][0].Equals(codeABZ));
             Assert.IsTrue(paths[0][1].Equals(codeSOF));
-            
-            paths = generator.GeneratePaths(codeABZ, codeSOF, 3);
-            Assert.IsTrue(paths.Count == 2);
+        }
+
+        [TestMethod]
+        public void GetAirportConnections_ReturnsExpectedThreePathsWhenMaxFlightsAreTwo()
+        {
+            List<List<string>> paths = generator.GeneratePaths(codeABZ, codeSOF, 2);
+            Assert.IsTrue(paths.Count == 3);
             Assert.IsTrue(paths[0].Count == 2);
             Assert.IsTrue(paths[0][0].Equals(codeABZ));
             Assert.IsTrue(paths[0][1].Equals(codeSOF));
-            Assert.IsTrue(paths[1].Count == 4);
+            Assert.IsTrue(paths[1].Count == 3);
             Assert.IsTrue(paths[1][0].Equals(codeABZ));
-            Assert.IsTrue(paths[1][1].Equals(codeSOF));
-            Assert.IsTrue(paths[1][2].Equals(codeEDI));
-            Assert.IsTrue(paths[1][3].Equals(codeSOF));
+            Assert.IsTrue(paths[1][1].Equals(codeEDI));
+            Assert.IsTrue(paths[1][2].Equals(codeSOF));
+            Assert.IsTrue(paths[2].Count == 3);
+            Assert.IsTrue(paths[2][0].Equals(codeABZ));
+            Assert.IsTrue(paths[2][1].Equals(codeCIA));
+            Assert.IsTrue(paths[2][2].Equals(codeSOF));
+        }
+        
+        [TestMethod]
+        public void GetAirportConnections_ReturnsExpectedThreePathsWhenMaxFlightsAreThree()
+        {
+            List<List<string>> paths = generator.GeneratePaths(codeABZ, codeSOF, 3);
+            Assert.IsTrue(paths.Count == 5);
+            //Assert.IsTrue(paths[0].Count == 5);
+            //Assert.IsTrue(paths[0][0].Equals(codeABZ));
+            //Assert.IsTrue(paths[0][1].Equals(codeSOF));
+            //Assert.IsTrue(paths[1].Count == 3);
+            //Assert.IsTrue(paths[1][0].Equals(codeABZ));
+            //Assert.IsTrue(paths[1][1].Equals(codeEDI));
+            //Assert.IsTrue(paths[1][2].Equals(codeSOF));
+            //Assert.IsTrue(paths[2].Count == 3);
+            //Assert.IsTrue(paths[2][0].Equals(codeABZ));
+            //Assert.IsTrue(paths[2][1].Equals(codeCIA));
+            //Assert.IsTrue(paths[2][2].Equals(codeSOF));
         }
     }
 }
