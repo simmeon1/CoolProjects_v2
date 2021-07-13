4f9453d757052a8cd74a62f29696519dea27e7fb
simmeon1 <simmeonn1@gmail.com>
2021-07-13 23:10:06 +0100

    Cleanup

--- LeagueAPI_Tests/LeagueAPI_UnitTests.cs
+++ LeagueAPI_Tests/LeagueAPI_UnitTests.cs
@@ -12,16 +12,14 @@ namespace LeagueAPI_Tests
     [TestClass]
     public class LeagueAPI_UnitTests
     {
-        private const string id = "1";
-        private const string accountId = "2";
-        private const string puuid = "3";
-        private const string name = "4";
         private Account Account { get; set; }
+        private Mock<IHttpClient> ClientMock { get; set; }
 
         [TestInitialize]
         public void TestInitialize()
         {
-            Account = new(id, accountId, puuid, name);
+            Account = new("1", "2", "3", "4");
+            ClientMock = new();
         }
 
         [TestMethod]
@@ -30,16 +28,15 @@ namespace LeagueAPI_Tests
             HttpResponseMessage response = new(HttpStatusCode.OK);
             response.Content = new StringContent(
                 @"{
-                    'id': '" + id + @"',
-                    'accountId': '" + accountId + @"',
-                    'puuid': '" + puuid + @"',
-                    'name': '" + name + @"'
+                    'id': '" + Account.Id + @"',
+                    'accountId': '" + Account.AccountId + @"',
+                    'puuid': '" + Account.Puuid + @"',
+                    'name': '" + Account.Name + @"'
                 }"
             );
-            Mock<IHttpClient> httpClientMock = new();
-            httpClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()).Result).Returns(response);
+            ClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()).Result).Returns(response);
 
-            LeagueAPIClient leagueClient = await LeagueAPIClient.GetClientInstance(httpClientMock.Object, "someKey", "someName");
+            LeagueAPIClient leagueClient = await LeagueAPIClient.GetClientInstance(ClientMock.Object, "someKey", "someName");
             Assert.IsTrue(leagueClient.Account.Id.Equals(Account.Id));
             Assert.IsTrue(leagueClient.Account.AccountId.Equals(Account.AccountId));
             Assert.IsTrue(leagueClient.Account.Puuid.Equals(Account.Puuid));
@@ -51,17 +48,16 @@ namespace LeagueAPI_Tests
         {
             HttpResponseMessage response = new(HttpStatusCode.BadRequest);
             response.Content = new StringContent("");
-            Mock<IHttpClient> httpClientMock = new();
-            httpClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()).Result).Returns(response);
-            await Assert.ThrowsExceptionAsync<InvalidOperationException>(() => LeagueAPIClient.GetClientInstance(httpClientMock.Object, "", ""));
+            ClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()).Result).Returns(response);
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(() => LeagueAPIClient.GetClientInstance(ClientMock.Object, "", ""));
         }
 
         [TestMethod]
         public async Task GetMatches_GetsResults()
         {
-            HttpResponseMessage response = new(HttpStatusCode.OK);
             string matchId1 = "EUW1_5364680752";
             string matchId2 = "EUW1_5357084019";
+            HttpResponseMessage response = new(HttpStatusCode.OK);
             response.Content = new StringContent(
                 @"[
                     '" + matchId1 + @"',
@@ -69,9 +65,8 @@ namespace LeagueAPI_Tests
                   ]"
             );
 
-            Mock<IHttpClient> httpClientMock = new();
-            httpClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()).Result).Returns(response);
-            LeagueAPIClient leagueClient = LeagueAPIClient.GetClientInstance(httpClientMock.Object, "someKey", Account);
+            ClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()).Result).Returns(response);
+            LeagueAPIClient leagueClient = LeagueAPIClient.GetClientInstance(ClientMock.Object, "someKey", Account);
             List<string> matchIds = await leagueClient.GetMatchIds(450);
             Assert.IsTrue(matchIds.Count == 2);
             Assert.IsTrue(matchIds[0].Contains(matchId1));
