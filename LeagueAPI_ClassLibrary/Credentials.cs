47f2dd5dc351f0ed2a796681b99e652d8fe8e68e
simmeon1 <simmeonn1@gmail.com>
2021-07-16 14:16:10 +0100

    Added credentials

---
+++ LeagueAPI_ClassLibrary/Credentials.cs
@@ -0,0 +1,9 @@
+﻿namespace LeagueAPI_ClassLibrary
+{
+    public class Credentials
+    {
+        public string AccountName { get; set; }
+        public string AccountPuuid { get; set; }
+        public string Token { get; set; }
+    }
+}
--- LeagueAPI_Tests/LeagueAPI_IntegrationTests.cs
+++ LeagueAPI_Tests/LeagueAPI_IntegrationTests.cs
@@ -1,8 +1,10 @@
 using LeagueAPI_ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,8 +16,7 @@ namespace LeagueAPI_Tests
     public class LeagueAPI_IntegrationTests
     {
         public TestContext TestContext { get; set; }
-        private string Token { get; set; }
-        private string AccountName { get; set; }
+        private Credentials Credentials { get; set; }
         private IHttpClient HttpClient { get; set; }
 
         public LeagueAPI_IntegrationTests()
@@ -26,16 +27,15 @@ namespace LeagueAPI_Tests
         [TestInitialize]
         public void TestInitialize()
         {
-            Token = (string)TestContext.Properties["riotToken"];
-            AccountName = (string)TestContext.Properties["accountName"];
+            Credentials = JsonConvert.DeserializeObject<Credentials>(File.ReadAllText((string)TestContext.Properties["credentialsPath"]));
         }
 
         [TestMethod]
         public async Task GetAccountBySummonerName_KeyIsValid()
         {
-            LeagueAPIClient leagueClient = new(HttpClient, Token);
-            Account acc = await leagueClient.GetAccountBySummonerName(AccountName);
-            Assert.IsTrue(acc.Name.Equals(AccountName));
+            LeagueAPIClient leagueClient = new(HttpClient, Credentials.Token);
+            Account acc = await leagueClient.GetAccountBySummonerName(Credentials.AccountName);
+            Assert.IsTrue(acc.Name.Equals(Credentials.AccountName));
             Assert.IsTrue(acc.AccountId.Length > 0);
             Assert.IsTrue(acc.Id.Length > 0);
             Assert.IsTrue(acc.Puuid.Length > 0);
