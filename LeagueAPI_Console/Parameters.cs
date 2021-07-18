4970af40c60c60e6f060576df46ed21ab69e14a0
simmeon1 <simmeonn1@gmail.com>
2021-07-19 00:51:51 +0100

    Added league console app

---
+++ LeagueAPI_Console/Parameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueAPI_Console
+{
+    public class Parameters
+    {
+        public string Token { get; set; }
+        public string DdragonJsonFilesDirectoryPath { get; set; }
+        public string OutputDirectory { get; set; }
+        public string TargetVersion { get; set; }
+        public int QueueId { get; set; }
+        public string AccountPuuid { get; set; }
+        public int MaxCount { get; set; }
+    }
+}
---
+++ LeagueAPI_Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Common_ClassLibrary;
+using LeagueAPI_ClassLibrary;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeagueAPI_Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string parametersPath = "";
+            foreach (string arg in args)
+            {
+                Match match = Regex.Match(arg, "parametersPath-(.*)");
+                if (match.Success) parametersPath = match.Groups[1].Value;
+            }
+            Parameters parameters = File.ReadAllText(parametersPath).DeserializeObject<Parameters>();
+            RealHttpClient http = new();
+            Delayer delayer = new();
+            Logger_Console logger = new();
+            LeagueAPIClient client = new(http, parameters.Token, delayer, logger);
+            MatchCollector collector = new(client, logger);
+            DdragonRepository repo = new(parameters.DdragonJsonFilesDirectoryPath);
+            FullRunner runner = new(collector, repo);
+            List<string> files = await runner.DoFullRun(parameters.OutputDirectory, parameters.QueueId, parameters.AccountPuuid, targetVersion: parameters.TargetVersion, maxCount: parameters.MaxCount);
+            if (files.Count > 0) logger.Log($"{files.Count} files written at {parameters.OutputDirectory}. Press any key to exit." );
+            Console.ReadKey();
+        }
+    }
+}
