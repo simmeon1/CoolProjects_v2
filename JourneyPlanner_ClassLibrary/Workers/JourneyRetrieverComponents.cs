31dba4e7134307e190361550432e093ab905a473
simmeon1 <simmeonn1@gmail.com>
2022-03-07 10:02:09 +0000

    Improved exception handling

--- JourneyPlanner_ClassLibrary/Workers/JourneyRetrieverComponents.cs
+++ JourneyPlanner_ClassLibrary/Workers/JourneyRetrieverComponents.cs
@@ -10,13 +10,16 @@ namespace JourneyPlanner_ClassLibrary.Workers
 {
     public class JourneyRetrieverComponents
     {
-        public IWebDriver Driver { get; set; }
-        public ILogger Logger { get; set; }
-        public IDelayer Delayer { get; set; }
-        public IJavaScriptExecutor JavaScriptExecutor { get; set; }
-        public IWebDriverWaitProvider WebDriverWaitProvider { get; set; }
-        public IHttpClient HttpClient { get; set; }
-        public JourneyRetrieverComponents(IWebDriver driver, ILogger logger, IWebDriverWaitProvider webDriverWaitProvider, IDelayer delayer, IHttpClient httpClient, IJavaScriptExecutor javaScriptExecutor)
+        public IWebDriver Driver { get; }
+        public ILogger Logger { get; }
+        public IDelayer Delayer { get; }
+        public IJavaScriptExecutor JavaScriptExecutor { get; }
+        public IWebDriverWaitProvider WebDriverWaitProvider { get; }
+        public IHttpClient HttpClient { get; }
+
+        public JourneyRetrieverComponents(IWebDriver driver, ILogger logger,
+            IWebDriverWaitProvider webDriverWaitProvider, IDelayer delayer, IHttpClient httpClient,
+            IJavaScriptExecutor javaScriptExecutor)
         {
             Driver = driver;
             Logger = logger;
@@ -30,7 +33,7 @@ namespace JourneyPlanner_ClassLibrary.Workers
         {
             Logger.Log(message);
         }
-        
+
         public async Task Delay(int milliseconds)
         {
             await Delayer.Delay(milliseconds);
@@ -41,7 +44,7 @@ namespace JourneyPlanner_ClassLibrary.Workers
             INavigation navigation = Driver.Navigate();
             navigation?.GoToUrl(url);
         }
-        
+
         public bool ElementsContainsClass(IWebElement element, string targetCls)
         {
             string[] elementClasses = element.GetAttribute("class").Split(" ", StringSplitOptions.RemoveEmptyEntries);
@@ -49,10 +52,12 @@ namespace JourneyPlanner_ClassLibrary.Workers
             {
                 if (cls != null && cls.Equals(targetCls)) return true;
             }
+
             return false;
         }
 
-        public IWebElement FindElementByAttribute(By by, string attribute = "innerText", string text = "", ISearchContext container = null, int indexOfElement = -1, int seconds = 10)
+        public IWebElement FindElementByAttribute(By by, string attribute = "innerText", string text = "",
+            ISearchContext container = null, int indexOfElement = -1, int seconds = 10)
         {
             IWebElement element = WebDriverWaitProvider.Until(d =>
             {
@@ -72,47 +77,57 @@ namespace JourneyPlanner_ClassLibrary.Workers
                         }
                         return null;
                     }
-                    catch (WebDriverTimeoutException ex)
-                    {
-                        throw;
-                    }
                     catch (Exception ex)
                     {
-                        Log("Error with finding element by attribute. Details:");
-                        Log(ex.ToString());
-                        if (fails++ == 100) throw;
+                        HandleException(ex, ref fails, "Error with finding element by attribute.");
                     }
                 }
             }, seconds);
             return element;
         }
 
-        public IWebElement FindElementByAttributeAndClickIt(By by, string attribute = "innerText", string text = "", ISearchContext container = null, int indexOfElement = -1, bool clickElement = true, int seconds = 10)
+        private void HandleException(Exception ex, ref int fails, string message)
+        {
+            if (ex is WebDriverTimeoutException) throw ex;
+            if (ex.Message.Contains("Only one usage of each socket address"))
+            {
+                const int milliseconds = 120000;
+                Log(
+                    $"Sockets used up. Waiting {TimeSpan.FromMilliseconds(milliseconds).Minutes} minutes. Continues at {DateTime.Now.AddMilliseconds(milliseconds).ToString()}");
+                Delayer.Sleep(milliseconds);
+                return;
+            }
+
+            if (fails++ < 100) return;
+            Log($"{message}. Details:");
+            Log(ex.ToString());
+            throw ex;
+        }
+
+        public IWebElement FindElementByAttributeAndClickIt(By by, string attribute = "innerText", string text = "",
+            ISearchContext container = null, int indexOfElement = -1, bool clickElement = true, int seconds = 10)
         {
             int fails = 0;
             while (true)
             {
                 try
                 {
-                    IWebElement element = FindElementByAttribute(by, attribute, text, container, indexOfElement, seconds);
+                    IWebElement element =
+                        FindElementByAttribute(by, attribute, text, container, indexOfElement, seconds);
                     WebDriverWaitProvider.Until(WebDriverWaitProvider.ElementIsClickable(element));
                     if (clickElement) element.Click();
                     return element;
                 }
-                catch (WebDriverTimeoutException ex)
-                {
-                    throw;
-                }
                 catch (Exception ex)
                 {
-                    Log("Error with finding element by attribute and clicking. Details:");
-                    Log(ex.ToString());
-                    if (fails++ == 100) throw;
+                    HandleException(ex, ref fails, "Error with finding element by attribute and clicking.");
                 }
             }
         }
 
-        public IWebElement FindElementByAttributeAndSendKeysToIt(By by, string attribute = "innerText", string text = "", ISearchContext container = null, int indexOfElement = -1, List<string> keys = null, bool doClearFirst = true, int seconds = 10)
+        public IWebElement FindElementByAttributeAndSendKeysToIt(By by, string attribute = "innerText",
+            string text = "", ISearchContext container = null, int indexOfElement = -1, List<string> keys = null,
+            bool doClearFirst = true, int seconds = 10)
         {
             keys ??= new List<string>();
             int fails = 0;
@@ -120,7 +135,8 @@ namespace JourneyPlanner_ClassLibrary.Workers
             {
                 try
                 {
-                    IWebElement element = FindElementByAttribute(by, attribute, text, container, indexOfElement, seconds);
+                    IWebElement element =
+                        FindElementByAttribute(by, attribute, text, container, indexOfElement, seconds);
                     if (doClearFirst) element.Clear();
                     foreach (string key in keys)
                     {
@@ -128,20 +144,15 @@ namespace JourneyPlanner_ClassLibrary.Workers
                     }
                     return element;
                 }
-                catch (WebDriverTimeoutException ex)
-                {
-                    throw;
-                }
                 catch (Exception ex)
                 {
-                    Log("Error with finding element and sending keys. Details:");
-                    Log(ex.ToString());
-                    if (fails++ == 100) throw;
+                    HandleException(ex, ref fails, "Error with finding element and sending keys.");
                 }
             }
         }
 
-        public ReadOnlyCollection<IWebElement> FindElementsNew(By by, string attribute = "innerText", string text = "", ISearchContext container = null, int seconds = 10)
+        public ReadOnlyCollection<IWebElement> FindElementsNew(By by, string attribute = "innerText", string text = "",
+            ISearchContext container = null, int seconds = 10)
         {
             ReadOnlyCollection<IWebElement> elements = WebDriverWaitProvider.Until(d =>
             {
@@ -164,15 +175,9 @@ namespace JourneyPlanner_ClassLibrary.Workers
                         }
                         return new ReadOnlyCollection<IWebElement>(results);
                     }
-                    catch (WebDriverTimeoutException ex)
-                    {
-                        throw;
-                    }
                     catch (Exception ex)
                     {
-                        Log("Error with finding multiple elements. Details:");
-                        Log(ex.ToString());
-                        if (fails++ == 100) throw;
+                        HandleException(ex, ref fails, "Error with finding multiple elements.");
                     }
                 }
             }, seconds);
