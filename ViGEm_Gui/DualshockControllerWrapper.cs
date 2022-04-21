9607fb49d3d137cfc4b72d8af5c5e510c2286d05
simmeon1 <simmeonn1@gmail.com>
2022-04-21 11:06:17 +0100

    Added state comparison

--- ViGEm_Gui/DualshockControllerWrapper.cs
+++ ViGEm_Gui/DualshockControllerWrapper.cs
@@ -13,6 +13,7 @@ namespace ViGEm
     public class DualshockControllerWrapper
     {
         private IDualShock4Controller controller;
+        private HtmlControllerState lastState = new();
 
         public DualshockControllerWrapper(IDualShock4Controller controller)
         {
@@ -22,32 +23,33 @@ namespace ViGEm
         public void SetStateFromHtmlControllerState(HtmlControllerState state)
         {
             //lx
-            controller.SetAxisValue(0, state.A0);
+            if (state.A0 != lastState.A0) controller.SetAxisValue(0, state.A0);
             //ly
-            controller.SetAxisValue(1, state.A1);
+            if (state.A1 != lastState.A1) controller.SetAxisValue(1, state.A1);
             //rx
-            controller.SetAxisValue(2, state.A2);
+            if (state.A2 != lastState.A2) controller.SetAxisValue(2, state.A2);
             //ry
-            controller.SetAxisValue(3, state.A3);
-            controller.SetButtonState(DualShock4Button.Cross, state.B0);
-            controller.SetButtonState(DualShock4Button.Circle, state.B1);
-            controller.SetButtonState(DualShock4Button.Square, state.B2);
-            controller.SetButtonState(DualShock4Button.Triangle, state.B3);
-            controller.SetButtonState(DualShock4Button.ShoulderLeft, state.B4);
-            controller.SetButtonState(DualShock4Button.ShoulderRight, state.B5);
-            controller.SetButtonState(DualShock4Button.TriggerLeft, state.B6);
-            controller.SetButtonState(DualShock4Button.TriggerRight, state.B7);
-            controller.SetButtonState(DualShock4Button.Share, state.B8);
-            controller.SetButtonState(DualShock4Button.Options, state.B9);
-            controller.SetButtonState(DualShock4Button.ThumbLeft, state.B10);
-            controller.SetButtonState(DualShock4Button.ThumbRight, state.B11);
-            if (state.B12) controller.SetDPadDirection(DualShock4DPadDirection.North);
-            else if (state.B13) controller.SetDPadDirection(DualShock4DPadDirection.South);
-            else if (state.B14) controller.SetDPadDirection(DualShock4DPadDirection.West);
-            else if (state.B15) controller.SetDPadDirection(DualShock4DPadDirection.East);
+            if (state.A3 != lastState.A3) controller.SetAxisValue(3, state.A3);
+            if (state.B0 != lastState.B0) controller.SetButtonState(DualShock4Button.Cross, state.B0);
+            if (state.B1 != lastState.B1) controller.SetButtonState(DualShock4Button.Circle, state.B1);
+            if (state.B2 != lastState.B2) controller.SetButtonState(DualShock4Button.Square, state.B2);
+            if (state.B3 != lastState.B3) controller.SetButtonState(DualShock4Button.Triangle, state.B3);
+            if (state.B4 != lastState.B4) controller.SetButtonState(DualShock4Button.ShoulderLeft, state.B4);
+            if (state.B5 != lastState.B5) controller.SetButtonState(DualShock4Button.ShoulderRight, state.B5);
+            if (state.B6 != lastState.B6) controller.SetButtonState(DualShock4Button.TriggerLeft, state.B6);
+            if (state.B7 != lastState.B7) controller.SetButtonState(DualShock4Button.TriggerRight, state.B7);
+            if (state.B8 != lastState.B8) controller.SetButtonState(DualShock4Button.Share, state.B8);
+            if (state.B9 != lastState.B9) controller.SetButtonState(DualShock4Button.Options, state.B9);
+            if (state.B10 != lastState.B10) controller.SetButtonState(DualShock4Button.ThumbLeft, state.B10);
+            if (state.B11 != lastState.B11) controller.SetButtonState(DualShock4Button.ThumbRight, state.B11);
+            if (state.B12 != lastState.B12 && state.B12) controller.SetDPadDirection(DualShock4DPadDirection.North);
+            else if (state.B13 != lastState.B13 && state.B13) controller.SetDPadDirection(DualShock4DPadDirection.South);
+            else if (state.B14 != lastState.B14 && state.B14) controller.SetDPadDirection(DualShock4DPadDirection.West);
+            else if (state.B15 != lastState.B15 && state.B15) controller.SetDPadDirection(DualShock4DPadDirection.East);
             else controller.SetDPadDirection(DualShock4DPadDirection.None);
             // Controller.SetButtonState(DualShock4Button.ps, state.b16);
             // Controller.SetButtonState(DualShock4Button.touch, state.b17);
+            lastState = state;
         }
 
         public void SetButtonState(DualShock4Button button, bool pressed)
