f7ea2d0a96ddc1b2ea90763c233c1e86828a7e21
simmeon1 <simmeonn1@gmail.com>
2025-06-23 11:55:59 +0100

    PSM (page segmentation mode) added

--- WindowsScreenReading/TesseractUseCase.cs
+++ WindowsScreenReading/TesseractUseCase.cs
@@ -10,13 +10,26 @@ public class TesseractUseCase
     public TesseractUseCase()
     {
         engine = new TesseractEngine(@"C:\D\Apps\Vigem_scripts\tessdata", "eng", EngineMode.Default);
-        // Prevent output as performance can get affected
+        // Use appropriate page segmentation mode to properly parse text
+        // Default mode is (3 Auto) regularly results in invalid box/coordinates errors which affects performance
+        // through regular logging.
+        // Default is set to 7 Single line as that are the most expected use cases.
+        // The mode can also be provided to the Process method
+        // https://pyimagesearch.com/2021/11/15/tesseract-page-segmentation-modes-psms-explained-how-to-improve-your-ocr-accuracy/
+        engine.DefaultPageSegMode = PageSegMode.SingleLine;
+        Console.WriteLine($"Using PSM (page segmentation mode) {engine.DefaultPageSegMode}");
+        
+        // Prevent debug output as performance can get affected
+        // Includes metadata/total count=0 messages when using single line psm
+        engine.SetVariable("debug_file", "NUL");
+        Console.WriteLine("Disabled tess debug output for performance benefits");
+        
+        // PSM setting fixed empty page and invalid box errors.
+        // If those start appearing, adding borders to the images might be a good solution.
         // This might be better fixed by adding borders (empty page + invalid box errors)
         // https://github.com/tesseract-ocr/tessdoc/blob/main/ImproveQuality.md#borders
         // https://github.com/tesseract-ocr/tesseract/issues/427
         // https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!msg/tesseract-ocr/v26a-RYPSOE/2Sppq61GBwAJ
-        Console.WriteLine("Disabling tess output for performance");
-        engine.SetVariable("debug_file", "NUL");
     }
 
     public string GetTextFromClient(
