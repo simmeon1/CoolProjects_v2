6a4cc816e2213427ed1503da0197e5f3e273fc8d
simmeon1 <simmeonn1@gmail.com>
2026-02-11 21:09:31 +0000

    draft

--- BadmintonFE/src/app/home-component/home-component.ts
+++ BadmintonFE/src/app/home-component/home-component.ts
@@ -1,13 +1,43 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
     selector: 'home-component',
-    imports: [],
+    imports: [
+        AsyncPipe
+    ],
     templateUrl: './home-component.html',
     styleUrl: './home-component.sass',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+    public state$!: Observable<State>;
+
+    constructor(private readonly httpClient: HttpClient) {
+    }
+
+    ngOnInit(): void {
+        this.state$ = this.httpClient.get<MatchupMap>("http://localhost:5287/?names=a&names=b&names=c&names=d&minGames=1&courtCount=1").pipe(
+            map((m): State => ({map: m}))
+        )
+    }
+}
+
+interface State {
+    map: MatchupMap
+}
+
+type MatchupMap = Record<string, Matchup>;
+
+interface Matchup {
+    pairing1: Pairing;
+    pairing2: Pairing;
+}
+
+interface Pairing {
+    player1: string;
+    player2: string;
 }
