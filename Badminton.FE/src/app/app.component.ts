d3339e6f795b0fc7344f6829af6f9200d922c56b
simmeon1 <simmeonn1@gmail.com>
2026-02-12 11:19:33 +0000

    states

--- Badminton.FE/src/app/app.component.ts
+++ Badminton.FE/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {CdkTextareaAutosize} from "@angular/cdk/text-field";
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatCheckbox} from "@angular/material/checkbox";
 import {MatButton} from "@angular/material/button";
-import {catchError, of, Subject, switchMap} from "rxjs";
+import {catchError, concat, map, Observable, of, Subject, switchMap} from "rxjs";
 import shuffle from "knuth-shuffle-seeded";
 import {toSignal} from "@angular/core/rxjs-interop";
 import {JsonPipe} from "@angular/common";
@@ -21,24 +21,9 @@ import {JsonPipe} from "@angular/common";
 export class App {
     public readonly form: FormGroup<Form>
     private readonly onSubmit$ = new Subject<void>();
-    public readonly lastResponse = toSignal(this.onSubmit$.pipe(
-        switchMap(() => {
-            const names = this.getControl('names').value.split('\n').map(n => n.trim());
-            if (this.getControl('shuffle').value) {
-                shuffle(names);
-            }
-            const params = new HttpParams({
-                fromObject: {
-                    names,
-                    minGames: this.getControl('minGames').value,
-                    courtCount: this.getControl('courtCount').value
-                }
-            });
-            return this.httpClient.get<MatchupMap>("http://localhost:5287/api/", {params}).pipe(
-                catchError(() => of('Something went wrong. Is the form valid?'))
-            )
-        })
-    ), {initialValue: {} as MatchupMap});
+    public readonly state = toSignal(this.onSubmit$.pipe(
+        switchMap((): Observable<State> => concat(of({}), this.fetch())),
+    ));
 
     public constructor(
         private readonly httpClient: HttpClient,
@@ -51,10 +36,11 @@ Delta
 Echo
 Foxtrot
 Golf`;
+        const digitRegex = /^\d+$/;
         this.form = this.formBuilder.nonNullable.group({
             names: [initialNames, Validators.required],
-            minGames: ['4', [Validators.required, Validators.pattern(/^\d+$/)]],
-            courtCount: ['1', [Validators.required, Validators.pattern(/^\d+$/)]],
+            minGames: ['4', [Validators.required, Validators.pattern(digitRegex)]],
+            courtCount: ['1', [Validators.required, Validators.pattern(digitRegex)]],
             shuffle: [false],
         })
     }
@@ -66,11 +52,30 @@ Golf`;
     public onSubmit() {
         this.onSubmit$.next();
     }
+
+    private fetch() {
+        const names = this.getControl('names').value.split('\n').map(n => n.trim());
+        if (this.getControl('shuffle').value) {
+            shuffle(names);
+        }
+        const params = new HttpParams({
+            fromObject: {
+                names,
+                minGames: this.getControl('minGames').value,
+                courtCount: this.getControl('courtCount').value
+            }
+        });
+        return this.httpClient.get<MatchupMap>("http://localhost:5287/api/", {params}).pipe(
+            map((r): State => ({map: r})),
+            catchError((): Observable<State> => of({error: 'Something went wrong. Is the form valid?'}))
+        )
+    }
 }
 
 
 interface State {
-    map: MatchupMap
+    map?: MatchupMap
+    error?: string
 }
 
 type MatchupMap = Record<string, Matchup>;
