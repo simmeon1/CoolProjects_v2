a69c4ef71762deaeb4edc15ece369b3e3b9551ba
simmeon1 <simmeonn1@gmail.com>
2026-02-12 13:25:16 +0000

    signal forms

--- Badminton.FE/src/app/app.component.ts
+++ Badminton.FE/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {HttpParams, httpResource} from "@angular/common/http";
 import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 import {CdkTextareaAutosize} from "@angular/cdk/text-field";
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {form, FormField, min, required} from "@angular/forms/signals";
 import {MatCheckbox} from "@angular/material/checkbox";
 import {MatButton} from "@angular/material/button";
 import shuffle from "knuth-shuffle-seeded";
@@ -10,52 +10,50 @@ import {JsonPipe} from "@angular/common";
 
 @Component({
     selector: 'app-root',
-    imports: [MatFormField, MatLabel, CdkTextareaAutosize, MatInput, ReactiveFormsModule, MatCheckbox, MatButton, JsonPipe],
+    imports: [MatFormField, MatLabel, CdkTextareaAutosize, MatInput, MatCheckbox, MatButton, JsonPipe, FormField],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class App {
-    public readonly form: FormGroup<Form>
-    private readonly fetchUrl = signal<string | undefined>(undefined);
-    public readonly state = httpResource<MatchupMap>(() => this.fetchUrl());
-
-    public constructor(private readonly formBuilder: FormBuilder) {
-        const initialNames = `Alfa
+    public readonly form = form(signal<Form>({
+            names: `Alfa
 Bravo
 Charlie
 Delta
 Echo
 Foxtrot
-Golf`;
-        const digitRegex = /^\d+$/;
-        this.form = this.formBuilder.nonNullable.group({
-            names: [initialNames, Validators.required],
-            minGames: ['4', [Validators.required, Validators.pattern(digitRegex)]],
-            courtCount: ['1', [Validators.required, Validators.pattern(digitRegex)]],
-            shuffle: [false],
-        })
-    }
+Golf`,
+            minGames: 4,
+            courtCount: 1,
+            shuffle: false
+        }), (schemaPath) => {
+            required(schemaPath.names);
+            required(schemaPath.minGames);
+            min(schemaPath.minGames, 1);
+            required(schemaPath.courtCount);
+            min(schemaPath.courtCount, 1);
+        }
+    );
+    private readonly fetchUrl = signal<string | undefined>(undefined);
+    public readonly state = httpResource<MatchupMap>(() => this.fetchUrl());
 
-    public getControl<T extends keyof Form>(key: T): Form[T] {
-        return this.form.controls[key];
-    }
-
-    public onSubmit() {
+    public onSubmit($event: SubmitEvent) {
+        $event.preventDefault();
         this.fetchUrl.set(this.getUrl());
     }
 
     private getUrl() {
-        const names = this.getControl('names').value.split('\n').map(n => n.trim());
-        if (this.getControl('shuffle').value) {
+        const names = this.form.names().value().split('\n').map(n => n.trim());
+        if (this.form.shuffle().value()) {
             shuffle(names);
         }
         const params = new HttpParams({
             fromObject: {
                 names,
-                minGames: this.getControl('minGames').value,
-                courtCount: this.getControl('courtCount').value
+                minGames: this.form.minGames().value(),
+                courtCount: this.form.courtCount().value()
             }
         });
         return `http://localhost:5287/api/?${params.toString()}`;
@@ -75,8 +73,8 @@ interface Pairing {
 }
 
 interface Form {
-    names: FormControl<string>;
-    minGames: FormControl<string>;
-    courtCount: FormControl<string>;
-    shuffle: FormControl<boolean>;
+    names: string;
+    minGames: number;
+    courtCount: number;
+    shuffle: boolean;
 }
