95c924f7c3bfe710a324fc5372e7dcf6e4013a38
simmeon1 <simmeonn1@gmail.com>
2026-02-13 17:53:31 +0000

    AllowAnyOrigin

--- Badminton.Api/Program.cs
+++ Badminton.Api/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     {
         options.AddPolicy(
             name: myAllowSpecificOrigins,
-            policy => { policy.WithOrigins("http://localhost:4200"); }
+            policy => { policy.AllowAnyOrigin(); }
         );
     }
 );
